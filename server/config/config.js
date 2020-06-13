2995ed6f0669ec561069ccba91a9f9c38e9cab51
Martin Barria Joost <martinjoostw@gmail.com>
2020-06-13 17:03:39 -0600

    envinroment var mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://martinbj:753s5NJQnJih6Zhh@cluster0-db3ev.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
